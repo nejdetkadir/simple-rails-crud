3794eb1663ab2516a866653542fee2e71a790f16
nejdetkadir <nejdetkadir.550@gmail.com>
2020-08-18 00:50:44 +0300

    Add phone update process

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,6 +1,11 @@
 class PhonesController < ApplicationController
+  before_action :set_phone, only: [:edit, :update, :destroy]
+
   def new
     @phone = Phone.new
+  end
+
+  def edit
   end
 
   def create
@@ -14,8 +19,21 @@ class PhonesController < ApplicationController
     end
   end
 
+  def update
+    if @phone.update(phone_params)
+      flash[:success] = "#{@phone.name} has been successfully updated."
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def phone_params
     params.require(:phone).permit(:name, :model, :year, :url, :price, :seller)
+  end
+
+  def set_phone
+    @phone = Phone.find(params[:id])
   end
 end
